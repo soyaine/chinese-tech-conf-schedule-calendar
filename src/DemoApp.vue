0a5eb77890688146305318659421dc77890a7dc8
Soyaine <soyaine@outlook.com>
2019-10-28 20:36:21 +0800

    enable list view

--- src/DemoApp.vue
+++ src/DemoApp.vue
@@ -1,10 +1,5 @@
 <template>
   <div class='demo-app'>
-    <div class='demo-app-top'>
-      <button @click="toggleWeekends">toggle weekends</button>
-      <button @click="gotoPast">go to a date in the past</button>
-      (also, click a date/time to add an event)
-    </div>
     <FullCalendar
       class='demo-app-calendar'
       ref="fullCalendar"
@@ -12,12 +7,15 @@
       :header="{
         left: 'prev,next today',
         center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        right: 'dayGridMonth, listMonth'
+      }"
+      :buttonText="{
+        listMonth: '📄️ 列表视图',
+        month: '📅️ 日历视图'
       }"
       :plugins="calendarPlugins"
       :weekends="calendarWeekends"
       :events="calendarEvents"
-      @dateClick="handleDateClick"
       />
   </div>
 </template>
@@ -26,6 +24,7 @@
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
 
 export default {
@@ -37,12 +36,18 @@ export default {
       calendarPlugins: [ // plugins must be defined in the JS
         dayGridPlugin,
         timeGridPlugin,
-        interactionPlugin // needed for dateClick
+        interactionPlugin, // needed for dateClick
+        listPlugin
       ],
       calendarWeekends: true,
       calendarEvents: [ // initial event data
-        { title: 'Event Now', start: new Date() }
-      ]
+        { title: 'QCon 全球软件开发大会 2019 上海站', start: '2019-10-17', end: '2019-10-20', url: 'https://qcon.infoq.cn/2019/shanghai/' },
+        { title: 'JavaScript 中国开发者大会 2019', start: '2019-10-19', end: '2019-10-21', url: 'https://2019.jsconfchina.com', backgroundColor: 'green' },
+        { title: '2019携程技术峰会', start: '2019-11-09', end: '2019-11-10', url: 'https://techsummit.ctrip.com', backgroundColor: 'green' },
+        { title: 'TOP100全球软件案例研究峰会 2019', start: '2019-11-14', end: '2019-11-19', url: 'https://www.top100summit.com', backgroundColor: 'green' },
+        { title: 'TWeb 腾讯前端技术大会 2019', start: '2019-11-16', end: '2019-11-17', url: 'https://tweb.tencent.com', backgroundColor: 'green' }
+      ],
+
     }
   },
   methods: {
@@ -74,18 +79,15 @@ export default {
 @import '~@fullcalendar/core/main.css';
 @import '~@fullcalendar/daygrid/main.css';
 @import '~@fullcalendar/timegrid/main.css';
+@import '~@fullcalendar/list/main.css';
 
 .demo-app {
   font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-  font-size: 14px;
-}
-
-.demo-app-top {
-  margin: 0 0 3em;
+  font-size: 16px;
 }
 
 .demo-app-calendar {
-  margin: 0 auto;
+  margin: 6em auto;
   max-width: 900px;
 }
 
